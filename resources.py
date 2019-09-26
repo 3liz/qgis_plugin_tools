291034d50248afd31eb483f9bda05e4044866070
Etienne Trimaille <gustrimaille@yahoo.fr>
2019-09-26 12:09:12 +0200

    add resources

---
+++ resources.py
@@ -0,0 +1,31 @@
+from os.path import abspath, join, pardir
+
+from qgis.PyQt import uic
+
+
+def resources_path(*args):
+    """Get the path to our resources folder.
+
+    :param args List of path elements e.g. ['img', 'logos', 'image.png']
+    :type args: str
+
+    :return: Absolute path to the resources folder.
+    :rtype: str
+    """
+    path = abspath(join(__file__, pardir, 'resources'))
+    for item in args:
+        path = abspath(join(path, item))
+
+    return path
+
+
+def load_ui(*args):
+    """Get compile UI file.
+
+    :param args List of path elements e.g. ['img', 'logos', 'image.png']
+    :type args: str
+
+    :return: Compiled UI file.
+    """
+    ui_class, _ = uic.loadUiType(resources_path('ui', *args))
+    return ui_class
