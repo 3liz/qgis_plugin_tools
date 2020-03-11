d69de6117e7510737fefcaa9f1768f990f4ff7f5
Etienne Trimaille <gustrimaille@yahoo.fr>
2020-03-11 10:42:10 +0100

    add version integer function fix #7

--- tools/version.py
+++ tools/version.py
@@ -3,6 +3,15 @@
 from .resources import metadata_config
 
 
+def format_version_integer(version_string: str):
+    """Transform version string to integers to allow comparing versions.
+
+    Transform "0.1.2" into "000102"
+    Transform "10.9.12" into "100912"
+    """
+    return int(''.join([a.zfill(2) for a in version_string.strip().split('.')]))
+
+
 def version() -> str:
     """Return the version defined in metadata.txt."""
     return metadata_config()['general']['version']
