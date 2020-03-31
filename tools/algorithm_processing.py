c940f40137792450917309ff69085a8808f12aee
Etienne Trimaille <gustrimaille@yahoo.fr>
2020-03-31 14:34:15 +0200

    add icon to Processing algorithm if the plugin is there

--- tools/algorithm_processing.py
+++ tools/algorithm_processing.py
@@ -1,6 +1,11 @@
 """Base class algorithm."""
+from os.path import isfile
 
 from qgis.core import QgsProcessingAlgorithm
+from qgis.PyQt.QtGui import QIcon
+
+from .resources import resources_path
+
 
 __copyright__ = "Copyright 2020, 3Liz"
 __license__ = "GPL version 3"
@@ -9,6 +14,7 @@ __revision__ = "$Format:%H$"
 
 
 class BaseProcessingAlgorithm(QgsProcessingAlgorithm):
+
     def __init__(self):
         super().__init__()
 
@@ -17,6 +23,13 @@ class BaseProcessingAlgorithm(QgsProcessingAlgorithm):
 
     def flags(self):
         return super().flags() | QgsProcessingAlgorithm.FlagHideFromModeler
+
+    def icon(self):
+        icon = resources_path('icons', 'icon.png')
+        if isfile(icon):
+            return QIcon(icon)
+        else:
+            return super().icon()
 
     def shortHelpString(self):
         raise NotImplementedError
